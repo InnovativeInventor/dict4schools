67c4363137cb0e37a754dd52061a7d55b052186d
InnovativeInventor <theinnovativeinventor@gmail.com>
2019-07-07 22:26:11 -0400

    Various updates

--- README.md
+++ README.md
@@ -11,19 +11,23 @@ Full - Removes anything remotely controversial, including some references to rel
 ## dict4schools strengths
 Simple has words that an ordinary person would know.
 Complex has most words in the English language (not including coined words)
-Full is a combination of simple and complex, with repeats taken out.
+Full is a combination of simple and complex, with repeats taken out .
+Uncensored is the entire dictionary uncensored, complete with my technical wordlist.
 
 ## Compiling
 Just run `bash blacklist.sh` in the dict4schools folder (make sure you've got all of the submodules). Make sure you have `pv` installed.
 
 ## Extras
-For spellcheckers (such as in Vim), use `spell/en.utf-8.add` which contains the full dict4schools list. An example config is as follows:
+For spellcheckers (such as in Vim), use `spell/en.utf-8.add` or `spell/uncensored/en.utf-8.add` which contains the full dict4schools and the uncensored list respectively. An example config is as follows:
 
 ```
 set spelllang=en
 set spellfile=~/git/dict4schools/spell/en.utf-8.add
 setlocal spell
+exec 'silent mkspell! ' &spellfile
 ```
+
+Use the last line if you want the spellfile to regenerate every time you open up Vim (highly useful for me when updating this repo).
 
 ## Credits & Contributions
 Contributions are always welcome! Just submit a pull request or create a new issue. This is in use in some projects, and any new additions to improve dict4schools are welcome.
--- compile.sh
+++ compile.sh
@@ -8,10 +8,10 @@ cd ../
 
 echo "Compiling safedict_simple.txt and safedict_complex.txt"
 echo "Progress:"
-pv sources/2of12.txt -i 1 | LC_ALL=C fgrep -i -F -w -v -f $blacklist | tee safedict_simple.txt >> safedict_full_temp.txt &
-pv sources/eff_wordlist.txt -i 1 | LC_ALL=C fgrep -i -F -w -v -f $blacklist | tee safedict_simple.txt >> safedict_full_temp.txt &
-pv sources/google-10000-english/20k.txt -i 1 | LC_ALL=C fgrep -i -F -w -v -f $blacklist | tee safedict_simple.txt >> safedict_full_temp.txt &
-pv sources/google-10000-english/google-10000-english.txt -i 1 | LC_ALL=C fgrep -i -F -w -v -f $blacklist | tee safedict_simple.txt >> safedict_full_temp.txt &
+pv sources/2of12.txt -i 1 | LC_ALL=C fgrep -i -F -w -v -f $blacklist | tee safedict_simple_temp.txt >> safedict_full_temp.txt &
+pv sources/eff_wordlist.txt -i 1 | LC_ALL=C fgrep -i -F -w -v -f $blacklist | tee safedict_simple_temp.txt >> safedict_full_temp.txt &
+pv sources/google-10000-english/20k.txt -i 1 | LC_ALL=C fgrep -i -F -w -v -f $blacklist | tee safedict_simple_temp.txt >> safedict_full_temp.txt &
+pv sources/google-10000-english/google-10000-english.txt -i 1 | LC_ALL=C fgrep -i -F -w -v -f $blacklist | tee safedict_simple_temp.txt >> safedict_full_temp.txt &
 pv sources/english-words/words_alpha.txt -i 1 | LC_ALL=C fgrep -i -F -w -v -f $blacklist | tee safedict_complex.txt  >> safedict_full_temp.txt &
 pv sources/personal.txt -i 1 | LC_ALL=C fgrep -i -F -w -v -f $blacklist | tee safedict_complex.txt  >> safedict_full_temp.txt &
 cat sources/2of12.txt > safedict_uncensored_temp.txt &
@@ -20,13 +20,14 @@ cat sources/google-10000-english/20k.txt >> safedict_uncensored_temp.txt &
 cat sources/google-10000-english/google-10000-english.txt >> safedict_uncensored_temp.txt &
 cat sources/english-words/words_alpha.txt >> safedict_uncensored_temp.txt &
 cat sources/personal.txt >> safedict_uncensored_temp.txt
+cat sources/technical.txt >> safedict_uncensored_temp.txt
 wait
 echo "Compiling safedict_full.txt"
 sort -u safedict_full_temp.txt > safedict_full.txt
+sort -u safedict_simple_temp.txt > safedict_simple.txt
 sort -u safedict_uncensored_temp.txt > safedict_uncensored.txt
 
-rm safedict_full_temp.txt
-rm safedict_uncensored_temp.txt
+rm *_temp.txt
 rm "*.xz"
 xz -k safedict_full.txt &
 xz -k safedict_complex.txt &
